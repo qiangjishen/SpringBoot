ae8447e848d7c946ce0dfcec30054673aa2c8638
qiangjishen <qiangjishen@gmail.com>
2019-01-28 16:26:39 +0800

    初始化

---
+++ src/test/java/cn/cnnic/sprngBoot2Famework/SprngBoot2FameworkApplicationTests.java
@@ -0,0 +1,17 @@
+package cn.cnnic.sprngBoot2Famework;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SprngBoot2FameworkApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
