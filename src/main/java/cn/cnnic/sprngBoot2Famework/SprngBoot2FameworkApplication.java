2db5bc42f17ebe772c4a97b28d25f655e6937380
qiangjishen <qiangjishen@gmail.com>
2019-01-28 16:45:48 +0800

    11

--- src/main/java/cn/cnnic/sprngBoot2Famework/SprngBoot2FameworkApplication.java
+++ src/main/java/cn/cnnic/sprngBoot2Famework/SprngBoot2FameworkApplication.java
@@ -2,14 +2,37 @@ package cn.cnnic.sprngBoot2Famework;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.converter.HttpMessageConverter;
+
+import com.alibaba.fastjson.serializer.SerializerFeature;
+import com.alibaba.fastjson.support.config.FastJsonConfig;
+import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
 
 @SpringBootApplication
 public class SprngBoot2FameworkApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SprngBoot2FameworkApplication.class, args);
+		SpringApplication app = new SpringApplication(SprngBoot2FameworkApplication.class);
+		//app.setBannerMode(Banner.Mode.OFF);
+		app.run(SprngBoot2FameworkApplication.class, args);
 	}
 
+	@Bean
+    public HttpMessageConverters fastJsonHttpMessageConverters(){
+        //1. 需要定义一个converter转换消息的对象
+        FastJsonHttpMessageConverter fasHttpMessageConverter = new FastJsonHttpMessageConverter();
+        
+        //2. 添加fastjson的配置信息，比如:是否需要格式化返回的json的数据
+        FastJsonConfig fastJsonConfig = new FastJsonConfig();
+        fastJsonConfig.setSerializerFeatures(SerializerFeature.PrettyFormat);
+        
+        //3. 在converter中添加配置信息
+        fasHttpMessageConverter.setFastJsonConfig(fastJsonConfig);
+        HttpMessageConverter<?> converter = fasHttpMessageConverter;
+        return new HttpMessageConverters(converter);
+    }
 	
 }
 
